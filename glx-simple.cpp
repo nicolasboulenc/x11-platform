c6f5ec90865920e5394de07622f167f7ec423862
Nicolas B <nicolasboulenc@gmail.com>
2023-03-26 12:11:36 +0100

    yet another commit

--- glx-simple.cpp
+++ glx-simple.cpp
@@ -2,17 +2,18 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>		// clock_gettime, nano_sleep
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
-
 #include <GL/gl.h>
 #include <GL/glx.h>
 
-#define WIDTH   800
-#define HEIGHT  600
+#define WIDTH   500
+#define HEIGHT  500
 
 int main() {
+
     Display *display = XOpenDisplay(NULL);
 
     int attr_list[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
@@ -42,10 +43,24 @@ int main() {
     glXMakeCurrent(display, window, gl_context);
 
     XStoreName(display, window, "OpenGL");
-
-    XSelectInput(display, window, KeyPressMask|KeyReleaseMask);
-
+    XSelectInput(display, window, KeyPressMask | KeyReleaseMask);
     XMapWindow(display, window);
+
+
+    glEnable(GL_TEXTURE_2D);
+
+    GLuint tex;
+    glGenTextures(1, &tex);
+
+    glBindTexture(GL_TEXTURE_2D, tex);
+
+	uint8_t *fb = (uint8_t *)malloc(4 * WIDTH * HEIGHT);
+	if (!fb) {
+		fputs("malloc()\n", stderr);
+		return -1;
+	}
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WIDTH, HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, fb);
+
 
     bool quit = false;
     while (!quit) {
@@ -63,11 +78,39 @@ int main() {
         glClearColor(0.15, 0.15, 0.15, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-            glColor3f(0.68, 0.84, 0.0);
-            glVertex2f(-0.5, -0.5);
-            glVertex2f( 0.5, -0.5);
-            glVertex2f( 0.0,  0.5);
+        glBindTexture(GL_TEXTURE_2D, tex);
+
+        clock_t now = clock();
+        int offx = now / (CLOCKS_PER_SEC / 500) % WIDTH;
+        int offy = now / (CLOCKS_PER_SEC / 500) % HEIGHT;
+        for (int y = 0; y < HEIGHT; y++) {
+            uint16_t r = ((y + offy) % 256);
+            for (int x = 0; x < WIDTH; x++) {
+                uint16_t b = ((x + offx) % 256);
+                int idx = (y * WIDTH + x) * 4;
+                fb[idx + 0] = b;
+                fb[idx + 1] = 0x00;
+                fb[idx + 2] = r;
+                fb[idx + 3] = 0xff;
+            }
+        }
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, WIDTH, HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, fb);
+
+        glNormal3f(0.0f,0.0f,1.0f);
+        // glColor3f(0.68, 0.84, 0.0); 
+
+        glBegin(GL_QUADS);
+            glTexCoord2f(-1.0f, -1.0f); 
+            glVertex2f(-0.5f, -0.5f); 
+
+            glTexCoord2f(1.0f, -1.0f); 
+            glVertex2f(0.5f, -0.5f); 
+
+            glTexCoord2f(1.0f, 1.0f); 
+            glVertex2f(0.5f, 0.5f); 
+
+            glTexCoord2f(-1.0f, 1.0f); 
+            glVertex2f(-0.5f, 0.5f);
         glEnd();
 
         glXSwapBuffers(display, window);
